7529089936dcdb050d5d870ed7bc987b28629eec
adridu59 <adri-from-59@hotmail.fr>
2012-02-19 22:32:11 +0100

    Edited find_file_in_dir loop to be sh compat

--- compile_bzip2.sh
+++ compile_bzip2.sh
@@ -4,10 +4,7 @@ fi
 mkdir temp
 cp -r NEO/* temp/.
 cd temp
-for i in $(find_file_in_dir .gitignore | grep './')
-do
-	rm $i
-done
+find -name '.gitignore' -exec rm {} \;
 find . | cpio -o -H newc | bzip2 > ../ramdisk.cpio.bz2
 cd ..
 rm -r temp
